9ed34e65f3b9206e91098361e7d5c1303d46a5f0
malireddymahesh <malireddymahesh111@gmail.com>
2023-12-15 12:48:22 +0530

    changes in the same line

--- cypress/e2e/1-getting-started/sample.cy.js
+++ cypress/e2e/1-getting-started/sample.cy.js
@@ -1,6 +1,6 @@
 describe("test suit",()=>{
     it("test cases",()=>{
     //write test scripts hear
-    //writren somthing in Tester1 branch
+    //writren somthing in Tester1 branch,test is written in different line
     })
 })
